b4bb66388495ca660f866e8e399ee259c158f60a
Zac Chung (DCS-TW) <zac_chung@trendmicro.com>
2018-10-21 05:10:48 +0800

    update

--- routes/typhoons.js
+++ routes/typhoons.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var router = express.Router();
 let { getDb } = require('../config/mongo');
+let bodyParser = require('body-parser');
 
 /* GET users listing. */
 router.get('/real', function(req, res, next) {
@@ -14,5 +15,19 @@ router.get('/real', function(req, res, next) {
       res.json(docs[0]);
     });
 });
+
+var jsonParser = bodyParser.json();
+
+router.post('/report', jsonParser, function(req, res, next) {
+  let db = getDb();
+  let ri = db.collection('report_info');
+  let body = req.body;
+  ri.insertOne(req.body, (err, r) => {
+    if (err) res.status(500).send(err.message);
+    else {
+      res.send();
+    }
+  });
+})
 
 module.exports = router;
