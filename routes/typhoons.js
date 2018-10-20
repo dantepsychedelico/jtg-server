eed8e03ac5c29f78318de33e112a10f9049e1406
Zac Chung (DCS-TW) <zac_chung@trendmicro.com>
2018-10-21 06:28:07 +0800

    update

--- routes/typhoons.js
+++ routes/typhoons.js
@@ -4,6 +4,7 @@ var express = require('express');
 var router = express.Router();
 let { getDb } = require('../config/mongo');
 let bodyParser = require('body-parser');
+let _ = require('lodash');
 
 /* GET users listing. */
 router.get('/real', function(req, res, next) {
@@ -16,12 +17,11 @@ router.get('/real', function(req, res, next) {
     });
 });
 
-var jsonParser = bodyParser.json();
-
-router.post('/report', jsonParser, function(req, res, next) {
+router.post('/report', function(req, res, next) {
   let db = getDb();
   let ri = db.collection('report_info');
   let body = req.body;
+  body.create_ts = new Date();
   ri.insertOne(req.body, (err, r) => {
     if (err) res.status(500).send(err.message);
     else {
@@ -29,5 +29,25 @@ router.post('/report', jsonParser, function(req, res, next) {
     }
   });
 })
+
+router.get('/report', function(req, res, next) {
+  let db = getDb();
+  let ri = db.collection('report_info');
+  ri.find({})
+    .sort({ 'create_ts': -1 })
+    .limit(10)
+    .toArray((err, docs) => {
+      if (err) console.log(err);
+      res.json(_.map(docs, (d) => {
+        return {
+          img: d.img,
+          lat: d.pos.coords.latitude,
+          lot: d.pos.coords.longitude,
+          desc: d.desc,
+          create_ts: d.create_ts
+        };
+      }));
+    });
+});
 
 module.exports = router;
